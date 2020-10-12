9ff93420c3d723d18edb7baa5bd93bb5a4bcd974
Anshul <anshul.sharma@nexxo.com>
2020-10-12 23:19:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect} from 'react';
+import Posts from './components/Posts';
+import Pagination from './components/Pagination';
+import Search from './components/Search'
+import axios from 'axios';
+import './App.css';
+
+const App = () => {
+  const ITEMS_PER_PAGE = 5;
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [search, setSearch] = useState("modi");
+  const [postsPerPage] = useState(ITEMS_PER_PAGE);
+
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://7ba3dbe5f8b0.ngrok.io/fetch/tweets/'+search);
+      console.log("response==>"+JSON.stringify(res.data.data.data));
+      setPosts(res.data.data.data);
+      setLoading(false);
+    };
+    console.log("searchinig..."+search);
+    fetchPosts();
+  }, [search]);
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
+  return (
+    <div className='container mt-5'>
+      <h1 className='text-primary mb-3'>Tweets</h1>
+      <Search
+              onSearch={value => {
+                  setSearch(value);
+                  setCurrentPage(1);
+              }}
+      />
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const Search = ({ onSearch }) => {
+    const [search, setSearch] = useState("modi");
+
+    const onInputChange = e => {
+        setSearch(e.target.value);
+    };
+
+    const getSearch = e => {
+        e.preventDefault();
+        onSearch(search);
+        setSearch('');
+    }
+    
+    return (
+        <form onSubmit={getSearch} className="search-form">
+            <input
+            type="text"
+            className="search-bar"
+            placeholder="Search by #hashtag"
+            value={search}
+            onChange={onInputChange}
+            />
+            <button className="search-button" type="submit">Submit</button>
+        </form>
+    );
+};
+
+export default Search;
